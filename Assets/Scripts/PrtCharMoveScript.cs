5f1b34bac38abbad129b43a9bbc5d1ef5734131c
DESKTOP-MCO0T94\t-Katsumata <t-katsumata@jaist.ac.jp>
2023-09-21 14:34:09 +0900

    [update]キャラクター移動1マス

--- Assets/Scripts/PrtCharMoveScript.cs
+++ Assets/Scripts/PrtCharMoveScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PrtCharMoveScript : MonoBehaviour
 {
+    [SerializeField] int X;
+    [SerializeField] int Y;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,13 @@ public class PrtCharMoveScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) { X -= 1; }
+        if (Input.GetKeyDown(KeyCode.RightArrow)) { X += 1; }
+        if (Input.GetKeyDown(KeyCode.UpArrow)) { Y += 1; }
+        if (Input.GetKeyDown(KeyCode.DownArrow)) { Y -= 1; }
+
+        Vector2 pos;
+        pos= new Vector2(X, Y);
+        transform.position = pos;
     }
 }
