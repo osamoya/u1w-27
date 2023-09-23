33cd6d1280d1eb433ff382217cc9d19a463270e0
DESKTOP-MCO0T94\t-Katsumata <t-katsumata@jaist.ac.jp>
2023-09-24 03:28:23 +0900

    [Update]パネル系でやりたいことはほぼ完成した

--- Assets/Scripts/Stage/CheckPanel_Script.cs
+++ Assets/Scripts/Stage/CheckPanel_Script.cs
@@ -7,10 +7,14 @@ public class CheckPanel_Script : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI ONOFF;
     [SerializeField] TextMeshProUGUI RedBall;
+    [SerializeField] TextMeshProUGUI End;
+    [SerializeField] bool noBall;
+    [SerializeField] bool noON;
+    [SerializeField] GameObject ClearPanel;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ClearPanel.SetActive(false);
     }
 
     // Update is called once per frame
@@ -19,22 +23,41 @@ public class CheckPanel_Script : MonoBehaviour
         if (Btn_Script.getBallNum()==0)
         {
             RedBall.text = "<s>Still red</s>";
+            noBall = true;
         }
         else
         {
             RedBall.text = "Still red";
+            noBall = false;
         }
         if (PrtStageManagerScript.isClear)
         {
             ONOFF.text = "<s>Still ON...</s>";
+            noON = true;
         }
         else
         {
             ONOFF.text = "Still ON...";
+            noON = false;
         }
-
+        if (noBall&&noON)
+        {
+            End.text = "Å´Clear!";
+        }
+        else
+        {
+            End.text = "Å´back to stageSelect";
+        }
         if (Input.GetKeyDown(KeyCode.DownArrow)){
-            SceneManager.LoadScene("SelectScene");
+            if (noBall && noON)
+            {
+                ClearPanel.SetActive(true);
+            }
+            else
+            {
+                SceneManager.LoadScene("SelectScene");
+            }
+            
         }
         
 
