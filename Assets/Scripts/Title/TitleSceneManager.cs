d52428a2eece279e63638c422a6b0ee63366659f
DESKTOP-MCO0T94\t-Katsumata <t-katsumata@jaist.ac.jp>
2023-09-24 14:42:42 +0900

    [Add]タイトルから遷移でフェードしつつSEがなる．マジックナンバーは後で調整しよう

--- Assets/Scripts/Title/TitleSceneManager.cs
+++ Assets/Scripts/Title/TitleSceneManager.cs
@@ -2,14 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class TitleSceneManager : MonoBehaviour
 {
     [SerializeField] string title;
+    [SerializeField] Image fadeOverRay;
+    [SerializeField] AudioClip fadeOutRay;
+    AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
-        
+         audioSource= this.GetComponent<AudioSource>();
+        fadeOverRay.DOFade(0,0);
     }
 
     // Update is called once per frame
@@ -17,13 +23,22 @@ public class TitleSceneManager : MonoBehaviour
     {
         if (Input.anyKeyDown&&!Input.GetMouseButton(0) && !Input.GetMouseButton(1))
         {
-            loadScene();
+            fadeTitle();
+            DOVirtual.DelayedCall(1,()=> loadScene());
+            
         }
     }
 
     private void loadScene()
     {
         SceneManager.LoadScene(title);
+    }
+    private void fadeTitle()
+    {
+        
+        
+        audioSource.PlayOneShot(fadeOutRay);
+        fadeOverRay.DOFade(1,0.2f);
     }
 
 }
