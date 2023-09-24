e75c052cdce64202e5572d3e7e74bce25c961f50
DESKTOP-MCO0T94\t-Katsumata <t-katsumata@jaist.ac.jp>
2023-09-24 18:03:20 +0900

    [Add]タイトルに必要な素材は入れた

--- Assets/Scripts/Title/TitleSceneManager.cs
+++ Assets/Scripts/Title/TitleSceneManager.cs
@@ -11,11 +11,21 @@ public class TitleSceneManager : MonoBehaviour
     [SerializeField] Image fadeOverRay;
     [SerializeField] AudioClip fadeOutRay;
     AudioSource audioSource;
+    [SerializeField] GameObject Rmcn_OFF;
+    [SerializeField] GameObject Rmcn_ON;
+    [SerializeField] GameObject Light_OFF;
+    [SerializeField] GameObject Light_ON;
+    [SerializeField] GameObject TV_OFF;
+    [SerializeField] GameObject TV_ON;
     // Start is called before the first frame update
     void Start()
     {
          audioSource= this.GetComponent<AudioSource>();
         fadeOverRay.DOFade(0,0);
+        setLight(true);
+        setTV(true);
+        setRmcn(false);
+        
     }
 
     // Update is called once per frame
@@ -23,6 +33,7 @@ public class TitleSceneManager : MonoBehaviour
     {
         if (Input.anyKeyDown&&!Input.GetMouseButton(0) && !Input.GetMouseButton(1))
         {
+            DoOP();
             fadeTitle();
             DOVirtual.DelayedCall(1,()=> loadScene());
             
@@ -40,5 +51,56 @@ public class TitleSceneManager : MonoBehaviour
         audioSource.PlayOneShot(fadeOutRay);
         fadeOverRay.DOFade(1,0.2f);
     }
+    private void DoOP()
+    {
+        audioSource.PlayOneShot(fadeOutRay);
+        //リモコン変更
+        setRmcn(true);
+        //両脇削除
+        DOVirtual.DelayedCall(1, () => setTV(false));
+        DOVirtual.DelayedCall(0, () => setLight(false));
+        //リモコン変更
+        setRmcn(false);
+        //暗転
 
+    }
+    private void setRmcn(bool b)
+    {
+        if (b)
+        {
+            Rmcn_OFF.SetActive(false);
+            Rmcn_ON.SetActive(true);
+        }
+        else
+        {
+            Rmcn_OFF.SetActive(true);
+            Rmcn_ON.SetActive(false);
+        }
+    }
+    private void setTV(bool b)
+    {
+        if (b)
+        {
+            TV_OFF.SetActive(false);
+            TV_ON.SetActive(true);
+        }
+        else
+        {
+            TV_OFF.SetActive(true);
+            TV_ON.SetActive(false);
+        }
+    }
+    private void setLight(bool b)
+    {
+        if (b)
+        {
+            Light_OFF.SetActive(false);
+            Light_ON.SetActive(true);
+        }
+        else
+        {
+            Light_OFF.SetActive(true);
+            Light_ON.SetActive(false);
+        }
+    }
 }
